57d88320af9e5a06d4507004f6e1734e1c36fc55
Reene <vernum.amor@gmail.com>
2023-07-23 14:35:33 +0900

    feature: update SmallButtonView
    add two buttons without background

--- SickSangHae/View/Reusable/SmallButtonView.swift
+++ SickSangHae/View/Reusable/SmallButtonView.swift
@@ -9,9 +9,43 @@ import SwiftUI
 
 struct SmallButtonView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button("먹었어요😋") {
+                print("click")
+            }
+            .buttonStyle(CustomButtonStyle())
+            .background(
+                Rectangle()
+                    .stroke(lineWidth: 0)
+                    .background(Color("PrimaryG"))
+                    .cornerRadius(15)
+            )
+            Spacer()
+            Button("상했어요🤢") {
+                print("click")
+            }
+            .buttonStyle(CustomButtonStyle())
+            .background(
+                Rectangle()
+                    .stroke(lineWidth: 0)
+                    .background(Color("SmallButton"))
+                    .cornerRadius(15)
+            )
+        }
+        .padding(.horizontal, 20)
     }
 }
+
+struct CustomButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .bold()
+            .frame(width: 167.adjusted, height: 60.adjusted)
+            .foregroundColor(.white)
+            .opacity(configuration.isPressed ? 0.7 : 1)
+    }
+}
+
 
 struct SmallButtonView_Previews: PreviewProvider {
     static var previews: some View {
