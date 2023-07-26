6cae3af7d0b8a728a4aa0a4bf35e00921d92b057
Reene <vernum.amor@gmail.com>
2023-07-26 14:18:12 +0900

    feature: update EditIconDetailView
    feature: update EditIconDetailView

--- SickSangHae/View/EditIconDetailView.swift
+++ SickSangHae/View/EditIconDetailView.swift
@@ -8,10 +8,57 @@
 import SwiftUI
 
 struct EditIconDetailView: View {
+    @State private var isShowingSheet = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Button(action: {
+            isShowingSheet.toggle()
+        }) {
+            Circle()
+                .frame(width: screenWidth * 0.08)
+                .foregroundColor(Color("Gray100"))
+                .overlay(Image(systemName: "pencil"))
+                .foregroundColor(Color("Gray800"))
+        }
+        .sheet(isPresented: $isShowingSheet,
+               onDismiss: didDismiss) {
+            
+            ScrollView {
+                Circle()
+                    .frame(width: screenWidth * 0.28)
+                    .foregroundColor(Color("Gray200"))
+                Text("계란 30구")
+                    .font(.title2)
+                    .bold()
+                    .padding(screenHeight * 0.028)
+                
+                LazyVGrid (
+                    columns: [GridItem(.adaptive(minimum: 70))]
+                ){
+                    ForEach(0..<36) { i in
+                        VStack {
+                            Circle()
+                                .fill(Color("Gray100"))
+                                .frame(width: screenWidth * 0.18)
+                            Text("얍")
+                        }
+                    }
+                }
+            }
+            
+            .padding(20)
+            
+            
+                Button("Dismiss",
+                       action: { isShowingSheet.toggle() })
+            }
+            
+        }
     }
-}
+    
+    
+    func didDismiss() {
+    }
 
 struct EditIconDetailView_Previews: PreviewProvider {
     static var previews: some View {
